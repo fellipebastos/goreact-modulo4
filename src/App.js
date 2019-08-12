51ec037ccba403620439c4d363cf4f6a1b14fa38
fellipebastos <fellipebastoswm@gmail.com>
2019-08-12 15:58:18 -0300

    images, favicon, styles and styled-components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from 'react';
 
+import GlobalStyle from './styles/global';
+
 function App() {
   return (
     <div className="App">
+      <GlobalStyle />
       <h1>Hello World!</h1>
     </div>
   );
